47c466f54843f0d8b48dbeb794a4f39ca1225df2
ayesha1906 <memonayesha458@gmail.com>
2020-04-05 21:38:51 +0530

    Methods of array

---
+++ ArrayFunctions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Arrays_Func
+{
+    class ArrayFunctions
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 3, 7, 9, 2, 14, 6 };
+            //length
+            Console.WriteLine("Length : "+ numbers.Length);  //non-static method rest all methods in program are static 
+
+            //Indexof
+            var index= Array.IndexOf(numbers, 9);
+            Console.WriteLine("Index of 9 : "+index);
+
+            //Clear()
+            Array.Clear(numbers, 0, 2);
+            Console.WriteLine("After Clear : ");
+            foreach(var n in numbers)
+                Console.WriteLine(n);
+
+            var str = new string[] { "John", "Julie", "elsa" ,"Anna"};
+            Array.Clear(str, 0, 3);
+            Console.WriteLine("After Clear : ");
+            foreach (var name in str)
+                Console.WriteLine(name);
+
+            //Copy()
+            var another = new int[3];
+            Array.Copy(numbers, another, 3);
+            Console.WriteLine("After Copy : ");
+            foreach (var n in another)
+                Console.WriteLine(n);
+
+            //Sort
+            Array.Sort(numbers);
+            Console.WriteLine("After Sort : ");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+
+            //Reverse
+            Array.Reverse(numbers);
+            Console.WriteLine("After Reverse : ");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+        }
+    }
+}
