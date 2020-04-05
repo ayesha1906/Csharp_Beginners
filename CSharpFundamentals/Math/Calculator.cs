a6eae047b8150ce592282664b98535f35fab0ab4
ayesha1906 <memonayesha458@gmail.com>
2020-04-05 09:48:37 +0530

    Classes and Objects

---
+++ CSharpFundamentals/Math/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace CSharpFundamentals.Math
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+
+        }
+    }
+}
---
+++ CSharpFundamentals/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using CSharpFundamentals.Math;
+
+
+namespace CSharpFundamentals
+{
+    public class Person
+    {
+        public string fname;
+        public string lname;
+
+        public void Introduction()
+        {
+            Console.WriteLine("My name is "+ fname +" " +lname + "!!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Person obj = new Person(); or can  use var to make code more cleaner
+            var obj = new Person();
+            obj.fname = "Ayesha";
+            obj.lname = "Memon";
+
+            obj.Introduction();
+
+            // Another class created in diff folder(Math) in same project
+            Calculator calci = new Calculator();
+            var result =calci.Add(5, 15);
+            Console.WriteLine("Addition = " +result);
+
+
+
+        }
+    }
+}
