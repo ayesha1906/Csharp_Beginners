0a3fb4d82f6d72543eb465b1430f388f4fc4a5f0
ayesha1906 <memonayesha458@gmail.com>
2020-04-05 09:52:51 +0530

    Refernce type and value type

---
+++ ReferenceAndValue/Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ReferenceAndValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //value types: making chnge in one will not impact other variable
+            int a = 10;                   
+            int b = a;
+            b++;
+            Console.WriteLine(string.Format("a: {0}, b: {1}  ", a, b));
+
+            //refence type: chnges in each othr will impact 
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+            array2[0] = 0;
+            foreach (var element in array2)
+                Console.Write( element+" ");
+            Console.WriteLine();
+
+            foreach (var element in array1)
+                Console.Write(element + " ");
+            Console.WriteLine();
+        }
+    }
+}
